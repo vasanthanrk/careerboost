673bd91710ccdf91c2cdb89a213ba7ec2830688d
vasanthanrk <vasanthan.raju@gmail.com>
2025-12-02 14:59:21 +0530

    log entry

--- resume-backend/app/middleware/tracking.py
+++ resume-backend/app/middleware/tracking.py
@@ -9,24 +9,24 @@ class TrackingMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
 
         ip = request.headers.get("CF-Connecting-IP") or request.client.host
-
-        # Get location using ipapi.co
-        country = "Unknown"
-        city = "Unknown"
-
-        try:
-            res = requests.get(f"https://ipapi.co/{ip}/json/").json()
-            country = res.get("country_name", "Unknown")
-            city = res.get("city", "Unknown")
-        except:
-            pass
-
-        user_agent = request.headers.get("User-Agent")
-
         db = SessionLocal()
         log = db.query(VisitorLog).filter(VisitorLog.ip == ip).first()
 
         if not log:
+            # Get location using ipapi.co
+            country = "Unknown"
+            city = "Unknown"
+
+            try:
+                res = requests.get(f"https://ipapi.co/49.47.199.135/json/").json()
+                print(res)
+                country = res.get("country_name", "Unknown")
+                city = res.get("city", "Unknown")
+            except:
+                pass
+
+            user_agent = request.headers.get("User-Agent")
+
             log = VisitorLog(ip=ip, country=country, city=city, user_agent=user_agent)
             db.add(log)
             db.commit()
